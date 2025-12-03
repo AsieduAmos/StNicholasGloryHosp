25857bf0e46388086debfb68a68cb473e0dc431a
Amos Asiedu <asieduamos6@gmail.com>
2025-12-03 00:30:34 +0000

    introduction of tailwind css and react components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MainBody from './components/MainBody.jsx';
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <MainBody />
+    
+    
+    
+    </>
+   
+  )
+}
+
+export default App
