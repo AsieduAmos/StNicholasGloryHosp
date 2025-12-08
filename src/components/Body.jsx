c17e0c50923fae19b20e83e012bff4f9d195b6d5
Amos Asiedu <asieduamos6@gmail.com>
2025-12-08 18:12:54 +0000

    stylling mission and vision container

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,45 +7,43 @@ const Body = () => {
 
   <>
     <section classNameName='font-sans text-lg font-normal leading-7'>
-          <div className="bg-slate-100 text-blue-600 font-bold text-xl text-center py-15 w-3/5 mx-auto mt-20 rounded-lg shadow-md">
+          <div className="bg-white text-blue-600 font-bold text-md text-center py-5 mx-20 mt-5 mb-30 rounded-lg shadow-lg">
             <p>
-              Health is the foundation upon which every other <br /> success is built, protect it with intention. <br /><br />
+              Health is the foundation upon which every other success is built, protect it with intention. <br /><br />
               We are here to help you live healthy and happy life.
             </p>
          </div>
     
-       <section className="flex w-3/4 h-96 bg-white text-blue-600 gap-6 justify-center-safe items-center mt-20 px-8 mx-auto 
-                          font-semibold border border-blue-500 rounded-xl">
-            <div className="mx-auto shadow-lg p-6 w-2/4">
-            <FaMapMarker className='inline' />
-                  <h2  className="font-bold text-3xl mb-10">Mission Statement</h2>
-                  <p> 
-                    To ensure that clints are treated with respect and <br /> great Care. <br />
-                    To ensure that the amblance of the hospital is always <br /> hygienic and
-                    fresh. <br /> To save and enhance clint's life.
+     <div className=' flex bg-slate-100 h-112 mx-20 mb-20 font-semibold text-lg text-center gap-5 rounded-xl '>
+      <div className='bg-white text-blue-600 w-1/3 ml-15 my-10 p-2 shadow-md rounded-lg'>
+        <h2 className='font-bold text-3xl my-10 '>Mission Statement</h2>
+        <p> 
+          To ensure that clints are treated with respect and  great Care.
+          To ensure that the amblance of the hospital is always hygienic and
+          fresh. To save and enhance clint's life.
                     
-                  </p>
-            </div>
-            <div className="mx-auto shadow-lg p-6 w-2/4">
-                  <h2 className="font-bold text-3xl mb-10 mx-auto">Our Vision</h2>
-                  <p>
-                    Promotion Of Health Care Delivery Through <br /> Prompt Services.
-                    Staff-Patient Relationship <br /> and High Excellence Staff Motivation.
-                  </p>
-            </div>
-        </section>
+        </p>
+      </div>
+
+      <div className='bg-red-500 text-white w-1/3  my-10 p-2 shadow-md rounded-lg'>
+         <h2 className='font-bold text-3xl my-10'>Our Vision</h2>
+         <p>
+            Promotion Of Health Care Delivery Through Prompt Services.
+            Staff-Patient Relationship and High Excellence Staff Motivation.
+          </p>
+      </div>
+
+     <div className='bg-white text-blue-600 w-1/3  my-10 mr-15 p-2 shadow-md rounded-lg'>
+       <h2 className='font-bold text-3xl my-10'>Our Core Vallues</h2>
+       <p>
+            Care <br /> Empathy <br /> Integrity <br /> Excellence <br /> Innovation <br /> Professionalism
+       </p>
+     </div>
+
+  </div>
  
-        <div className="bg-blue-500 text-white font-bold  text-center p-10 w-3/5 mx-auto mt-30 mb-30 border-3 border-red-900
-                          rounded-3xl shadow-md">
-                <h2  className="font-bold text-3xl mb-5">Our Core Vallues</h2>
-                <p className='text-xl'>
-                  Care <br /> Empathy <br /> Integrity <br /> Excellence <br /> Innovation <br /> Professionalism
-                </p>
-        </div>
-          
-    
-    </section>
-  </>
+   </section>
+</>
     
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,10 +9,10 @@ const NavBar = () => {
 <>
   <nav>
    <div className="container mx-auto">
-     <section className="h-25 grid grid-cols-3 bg-white text-blue-600  font-bold items-center border-b 
+     <section className="h-25 grid grid-cols-3 bg-white text-blue-600 font-bold items-center border-b 
                          border-blue-600 ">
-      <img className="w-22  ml-20 " src={logo}alt="logo"/>
-      <ul className="flex gap-6 text-xl cursor-pointer">
+      <img className="w-22 sm:ml-20 ml-10" src={logo}alt="logo"/>
+      <ul className="flex sm:gap-6 gap-4 sm:text-xl text-lg cursor-pointer">
         <li> 
           <Link className="border border-blue-500 px-3 py-1.5 rounded-lg  hover:text-blue-800 
                        hover:border-blue-800" to="/">Home</Link>
@@ -27,7 +27,7 @@ const NavBar = () => {
           <Link className="hover:text-blue-800" to="/NotFoundPage">Contact Us</Link>
         </li>
       </ul>
-      <ul className="bg-blue-600 text-white w-48 p-3 mx-auto ml-40 rounded-lg hover:bg-red-500 cursor-pointer">
+      <ul className="bg-blue-600 text-white sm:w-48 w-35 p-3 mx-auto sm:ml-40 ml-25 rounded-lg hover:bg-red-500 cursor-pointer">
         <li>
           <Link to="/NotFoundPage">Book Appointment</Link>
         </li>
