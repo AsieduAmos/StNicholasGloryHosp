7ce07e47a702ea43e5df13e939f9d4f4d2962065
Amos Asiedu <asieduamos6@gmail.com>
2026-04-02 15:48:07 +0000

    NavBar styled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,49 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { FaAddressBook } from 'react-icons/fa'; 
 import logo from '../assets/images/logo.jpg';
 
 
 const NavBar = () => {
+
+const activeStyle = ({ isActive }) => 
+    isActive ? 
+      ' px-3 py-1 rounded-md text-xl text-red-500 hover:text-red-700 cursor-pointer transition duration-300 ease-in-out'
+     : 
+     '  text-blue-500 hover:text-blue-900 cursor-pointer transition duration-300 ease-in-out';
+
   return (
 
 <>
   <nav>
-   <div className="container mx-auto">
-     <section className="h-24 grid grid-cols-3 bg-white text-blue-600 font-bold items-center border-b 
-                         border-blue-600 ">
-      <img className="w-22 sm:ml-20 ml-10" src={logo}alt="logo"/>
-      <ul className="flex sm:gap-6 gap-4 sm:text-xl text-lg cursor-pointer">
+   <div>
+     <section className="grid grid-cols-3 items-center border-b border-blue-700 mt-3 shadow-xl">
+      <img className="w-24 sm:ml-20 ml-10 p-2 pointer-events-none select-none" src={logo}alt="logo"/>
+
+      <ul className="flex gap-5 text-lg cursor-pointer">
         <li> 
-          <Link className="border border-blue-500 px-3 py-1.5 rounded-lg  hover:text-blue-800 
-                       hover:border-blue-800" to="/">Home</Link>
+          <NavLink className={ activeStyle }
+           to="/">Home
+           </NavLink>
         </li>
         <li> 
-          <Link className="hover:text-blue-800" to="services">Services</Link>
+          <NavLink className={ activeStyle } to="services">Services</NavLink>
         </li>
         <li>
-          <Link className="hover:text-blue-800" to="about">About Us</Link>
+          <NavLink className={ activeStyle } to="about">About Us</NavLink>
         </li>
         <li>
-          <Link className="hover:text-blue-800" to="contact">Contact Us</Link>
+          <NavLink className={ activeStyle } to="contact">Contact Us</NavLink>
         </li>
       </ul>
-      <ul className="bg-blue-600 text-white sm:w-48 w-35 p-3 mx-auto sm:ml-40 ml-25 rounded-lg hover:bg-red-500 cursor-pointer">
+
+      <ul>
         <li>
-          <Link to="/NotFoundPage">Book Appointment</Link>
+          <NavLink className="bg-blue-600 text-white sm:w-48 w-35 py-2 px-3 mx-auto sm:ml-40 ml-25 rounded-lg hover:bg-red-500 cursor-pointer" 
+           to=" ">
+             <FaAddressBook className='inline mr-1'/>
+            Book Appointment
+           </NavLink>
         </li>
       </ul>
      </section>
